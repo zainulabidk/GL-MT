f05a06f44b621a7c3abcd7b24737880e6a2c1fe3
zainmp <wwwzainzaan@gmail.com>
2025-03-17 12:21:11 +0530

    apii fixed

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://gl-mt-server.onrender.com/api/users";
+const API_URL = "https://gl-mt-server.onrender.com/api/auth/users";
 
 // const API_URL = "http://localhost:5000/api/auth/users";
 
